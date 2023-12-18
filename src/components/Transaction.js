5b1360a10d9e6b20a918d84cc63f5e426b1fc652
Victor <wanjala94vic@gmail.com>
2023-12-18 18:52:27 +0300

    updating components

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState';
 
 export const Transaction = ({transaction}) => {
+    const {deleteTransaction} = useContext(GlobalContext)
+
+
     const sign = transaction.amount < 0 ? '-':'+';
   return (
-    <li className="list-item minus">
+    <li className={transaction.amount <0? 'list-item minus':'list-item plus'}>
           <span className="transaction-description">
             {transaction.text}</span> 
             <span>{sign}${Math.abs(transaction.amount)}</span>
-          <button className="delete-btn">x</button>
+          <button onClick={()=> deleteTransaction(transaction.id)} className="delete-btn">x</button>
      </li>
   )
 }
